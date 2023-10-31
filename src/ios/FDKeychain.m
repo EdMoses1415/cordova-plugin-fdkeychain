cde0122a657e4fc99bf7ae8fdeedc1add4cead72
edwardmoses <41630531+edwardmoses@users.noreply.github.com>
2023-10-31 23:58:58 +0530

    Update FDKeychain.m

--- src/ios/FDKeychain.m
+++ src/ios/FDKeychain.m
@@ -111,6 +111,7 @@ NSString * const FDKeychainErrorDomain = @"com.1414degrees.keychain";
 
 	//Prepare the data to be stored
  	NSData *itemData =[item dataUsingEncoding:NSUTF8StringEncoding];
+  
 	//check if the item already exists
 	  NSDictionary *query = @{
 	    (__bridge id)kSecClass: (__bridge id)kSecClassGenericPassword,
@@ -147,7 +148,7 @@ NSString * const FDKeychainErrorDomain = @"com.1414degrees.keychain";
 	      (__bridge id)kSecClass: (__bridge id)kSecClassGenericPassword,
 	      (__bridge id)kSecAttrService: service,
 	      (__bridge id)kSecAttrAccessGroup: accessGroup,
-	      (__bridge id)kSecValueData: item
+	      (__bridge id)kSecValueData: itemData
 	    };
 	
 	    status = SecItemAdd((__bridge CFDictionaryRef)attributes,NULL);
